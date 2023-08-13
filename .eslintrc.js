773d0c3c99c06d9b7e300d640ec5b97b6ad306ce
Gerard Marquina Rubio (resolution GmbH) <g.marquina@resolution.de>
2023-08-13 13:59:52 +0200

    add linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,9 @@ module.exports = {
                 'node': true
             },
             'files': [
-                '.eslintrc.{js,cjs}'
+                '.eslintrc.{js,cjs}',
+                './src/*.ts',
+                './tests/*.ts',
             ],
             'parserOptions': {
                 'sourceType': 'script'
--- dist/cli.js
+++ dist/cli.js
@@ -1,21 +1,21 @@
 #!usr/bin/env node
-"use strict";
+'use strict';
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator['throw'](value)); } catch (e) { reject(e); } }
         function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_fs_1 = require("node:fs");
-const node_stream_1 = require("node:stream");
-const promises_1 = require("node:stream/promises");
-const csv_parse_1 = require("csv-parse");
-const csv_stringify_1 = require("csv-stringify");
-const logic_1 = require("./logic");
+Object.defineProperty(exports, '__esModule', { value: true });
+const node_fs_1 = require('node:fs');
+const node_stream_1 = require('node:stream');
+const promises_1 = require('node:stream/promises');
+const csv_parse_1 = require('csv-parse');
+const csv_stringify_1 = require('csv-stringify');
+const logic_1 = require('./logic');
 const parser = (0, csv_parse_1.parse)({
     fromLine: 2,
     skipEmptyLines: true,
